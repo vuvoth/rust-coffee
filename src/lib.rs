3e2c11192e328ebf9f1db241100cddd0cdf9340b
Vu Vo <vu.voth@gmail.com>
2022-05-04 01:01:12 +0700

    simple bubble sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
+mod bublesort {
+    pub fn bublesort(slice: &mut [i64]) {
+        let mut swaped = true;        
+        while swaped {
+            swaped = false;
+            for i in 1..slice.len() {
+                if slice[i - 1] > slice[i] {
+                    slice.swap(i - 1, i);
+                    swaped = true;
+                }
+            }
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
+    use super::bublesort::bublesort;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+
+        let mut arr = vec![1, 7, 3, -9, 5];
+        
+        bublesort(&mut arr);
+
+        assert_eq!(arr, vec![-9, 1, 3, 5, 7]);
     }
 }
