5910c50f00f7170e6ec4313ff06a09a05dd817d3
Vu Vo <vu.voth@gmail.com>
2022-05-05 03:19:52 +0700

    sort with generic type

--- src/sorts/bublesort.rs
+++ src/sorts/bublesort.rs
@@ -1,4 +1,4 @@
-pub fn bubblesort(slice: &mut [i64]) {
+pub fn bubblesort<T>(slice: &mut [T]) where T: Ord {
     let mut swaped = true;
     while swaped {
         swaped = false;
@@ -13,14 +13,24 @@ pub fn bubblesort(slice: &mut [i64]) {
 
 #[cfg(test)]
 mod test {
+    use super::bubblesort;
+
     #[test]
-    fn it_works() {
-        use super::bubblesort;
-        
+    fn it_works_with_i32_vector() {
+
         let mut arr = vec![1, 7, 3, -9, 5];
         
         bubblesort(&mut arr);
 
         assert_eq!(arr, vec![-9, 1, 3, 5, 7]);
+    }
+
+    #[test]
+    fn it_works_with_chars() {
+        let mut arr = vec!['a', 'e', 'c'];
+
+        bubblesort(&mut arr);
+
+        assert_eq!(arr, vec!['a', 'c', 'e']);
     }
 }
